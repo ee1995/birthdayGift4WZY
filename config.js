876faa26a18021ab956e2f5feba2a976dc7d9efa
ee1995 <123555451+ee1995@users.noreply.github.com>
2023-01-25 20:12:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,21 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "心爱的宝贝",  // 同上...
         "今天是你的生日",
         "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "第一个生日！",
+        "这两年来，我见过你",
+        "奇怪的样子",
+        "可爱的样子",
+        "生气的样子",
+        "都是我喜欢的样子",
+        "对了，礼物收全了吗？",
+        "今年要吃好好的",
+        "喂饱饱的",
+        "然后才有力气干活",
+        "和想我！",
+        "生日快乐~~",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,19 +30,33 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "biubiu 送给我": "./imgs/songxinxin.jpeg",
+        "心爱的宝贝": "./imgs/xinaibaobei.jpg",
+        "今天是你的生日": "./imgs/shengri.jpeg",
+        "这是我们在一起的": "./imgs/zaiyiqi.png",
+        "第一个生日！": "./imgs/nothing.jpg",
+        "这两年来，我见过你": "./imgs/yangzi.jpeg",
+        "奇怪的样子": "./imgs/nothing.jpg",
+        "可爱的样子": "./imgs/nothing.jpg",
+        "生气的样子": "./imgs/nothing.jpg",
+        "都是我喜欢的样子": "./imgs/nothing.jpg",
+        "对了，礼物收全了吗？": "./imgs/chuo.gif",
+        "今年要吃好好的": "./imgs/chihaodian.jpeg",
+        "喂饱饱的": "./imgs/weibao.jpeg",
+        "然后才有力气干活": "./imgs/nothing.jpg",
+        "和想我！": "./imgs/nothing.jpg",
+        "生日快乐~~": "./imgs/nothing.jpg"
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        turn_on: "PRESS ME",
+        play: "MUSICO STARTO~",
+        bannar_coming: "COLOURS",
+        balloons_flying: "AND SOMETHING MORE",
+        cake_fadein: "CAKE?",
+        light_candle: "CANDLES?",
         wish_message: "HAPPY BIRTHDAY~",
-        story: "A MESSAGE FOR YOU",
+        story: "A MESSAGE FOR YOU"
     },
     // 结束语
     loveText: '爱你的BABA 爱你的大宝贝~'
